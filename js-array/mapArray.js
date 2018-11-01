fb85e2b686a5c8f705c679e733676d091df06062
likui <likui@ttk.com>
2018-11-01 17:34:23 +0800

    数组常用api

--- js-array/mapArray.js
+++ js-array/mapArray.js
@@ -3,7 +3,7 @@
 //b: 当item为引用类型的时候，对item里的数据进行修改还是会改变原数组的
 //eq:
 let mapArr1 = [{
-  a: 10, b:20
+  a: 10, b: 20
 }]
 
 let mapArr2 = mapArr1.map(item => {
@@ -11,7 +11,7 @@ let mapArr2 = mapArr1.map(item => {
   return item
 })
 
-let mapArr3 = mapArr1.map(item => 
+let mapArr3 = mapArr1.map(item =>
   item.a = 30
 )
 
@@ -20,23 +20,24 @@ let mapArr3 = mapArr1.map(item =>
 // console.log(mapArr3) //[30]
 
 //附：实现原理
-Array.prototype.selfMap = function(fn, arr){
+Array.prototype.selfMap = function (fn, arr) {
   var newArr = []
-  for(var i = 0; i < this.length; i++){
-    if(arr){
+  for (var i = 0; i < this.length; i++) {
+    if (arr) {
       newArr.push(fn.call(arr, this[i], i, this))
-    }else{
-      newArr.push(fn(this[i],i,this))
+    } else {
+      newArr.push(fn(this[i], i, this))
     }
   }
   return newArr
 }
-let newdata = mapArr1.selfMap(function(item, index, arr){
-  console.log(this,arr)
+let newdata = mapArr1.selfMap(function (item, index, arr) {
+  console.log(this, arr)
   item.a = 100
-  return item 
-},mapArr1)
+  return item
+}, mapArr1)
 console.log(newdata)
+
 //2.forEach遍历
 // 修改原数组,无返回值
 let eachArr1 = [{
@@ -48,15 +49,77 @@ eachArr1.forEach(item => {
 // console.log(eachArr1) ///[{a: 50,b:20}]
 
 //附：实现原理
-Array.prototype.selfEach = function(fn, arr){
-  for(var i = 0; i < this.length; i++){
-    if(arr){
+Array.prototype.selfEach = function (fn, arr) {
+  for (var i = 0; i < this.length; i++) {
+    if (arr) {
       fn.call(arr, this[i], i, this)
-    }else{
+    } else {
       fn(this[i], i, this)
     }
   }
 }
-mapArr1.forEach(function(item,index,arr){
+mapArr1.forEach(function (item, index, arr) {
   console.log(item)
-},mapArr1)
+}, mapArr1)
+
+//3.Array.from
+//主要将类似数组的对象和可遍历（包含iterator接口）的对象转化为数组
+//类似数组的对象：本质特点是带有length属性
+let arrayLike = {
+  '0': 'a',
+  '1': 'b',
+  '2': 'c',
+  length: 3
+};
+
+// ES5的写法
+var arr1 = [].slice.call(arrayLike); // ['a', 'b', 'c']
+
+// ES6的写法
+let arr2 = Array.from(arrayLike); // ['a', 'b', 'c']
+Array.from('foo') //['f','o','o']
+function foo(){
+  const args = [...arguments];
+  return args
+}
+console.log(foo(1))
+Array.from([1,2,3], o => o*o) //类似于map，如果map函数里面用到了this关键字，还可以传入Array.from的第三个参数，用来绑定this
+
+//4.Array.of
+//Array.of总是返回参数值组成的数组。如果没有参数，就返回一个空数组
+//附：实现原理
+function ArrayOf(){
+  return [].slice.call(arguments);
+}
+console.log(ArrayOf(1,2,34))
+
+
+//5.数组实例的copyWithin
+//在当前数组内部，将指定位置的成员复制到其他位置（会覆盖原有成员），然后返回当前数组
+
+//6.find 用于找出第一个符合条件的数组成员,如果没有符合条件的成员，则返回undefined
+//7. findIndex 返回第一个符合条件的数组成员的位置，如果所有成员都不符合条件，则返回-1
+let letter = ['a', 'b', 'c'];
+let entries = letter.entries();
+console.log(entries.next().value); //['0','a']
+
+//8.includes 返回一个布尔值，表示某个数组是否包含给定的值(该方法的第二个参数表示搜索的起始位置，默认为0)
+[NaN].indexOf(NaN) //-1
+[NaN].includes(NaN) //true
+
+//9.flat 
+//用于将嵌套的数组“拉平”，变成一维的数
+//如果不管有多少层嵌套，都要转成一维数组，可以用Infinity关键字作为参数
+
+//10.some 对数组的每一项都运行给定的函数，有一项返回 ture,则返回 true
+function compare(element, index, array) {
+  return element > 10;
+}    
+[2, 5, 8, 1, 4].some(compare);  // false
+[12, 5, 8, 1, 4].some(compare); // true
+
+//11.对数组的每一项都运行给定的函数，每一项都返回 ture,则返回 true
+function isBigEnough(element, index, array) {
+  return element < 10;
+}    
+[2, 5, 8, 3, 4].every(isBigEnough);   // true
